7b19d082feed6baee1e3104fc9ac1ac835795202
socwiki <socijalizam.hrvatska@gmail.com>
2024-06-29 14:16:43 +0200

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,13 @@
+﻿    <aside>
+      <div class ="menu">
+        <details class="menu-item" open="">
+          <summary class="menu-item-summary">Izbornik</summary>
+          <div class="menu-item-content">
+            <ul>
+              <li><a href="index.html">Početna stranica</a></li>
+              <li><a href="faq.html">FAQ za ne-ljevičare</a></li>
+            </ul>
+          </div>
+        </details>
+      </div>
+    </aside>
