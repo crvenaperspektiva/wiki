a3d1c45236f8f0755b2e5b9a9eede23ed7112d93
crvenaperspektiva <socijalizam.hrvatska@gmail.com>
2024-07-02 21:46:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,3 +11,25 @@ class HeaderInc extends HTMLElement {
 }
 
 customElements.define('header-inc', HeaderInc)
+
+class Sidebar extends HTMLElement {
+	connectedCallback() {
+		this.innerHTML 0 `
+			<div class="sidebar">
+        			<div class ="menu">
+        				<details class="menu-item" open="">
+          					<summary class="menu-item-summary">Izbornik</summary>
+          						<div class="menu-item-content">
+            							<ul>
+              								<li><a href="index.html">Početna stranica</a></li>
+              								<li><a href="faq.html">FAQ za ne-ljevičare</a></li>
+            							</ul>
+          						</div>
+        				</details>
+      				</div>
+     			 </div>
+	 	`
+	}
+}
+
+customElements.define('sidebar', Sidebar)
